5ae85dcc4f63e9e0c0dd04e6a8843539c53f7d35
Tuncay Basak <tuncay.basak@outlook.com>
2024-01-30 11:27:21 +0100

    Upload

--- dist/factory/factory.js
+++ dist/factory/factory.js
@@ -1,31 +1,81 @@
 "use strict";
+/**
+ * Returns an abstract class that will add methods to manage instantiation and access to your class instances.
+ * Your instance will inherit the destroy method that will retrive the reference from the instance array so
+ * it would be deleted by the garbage collector as you make sure that there is no other refence to it.
+ *
+ * Your class has to implement init for what you wish to do at instantiation time as you will not use the constructor.
+ * @example
+ * class Foo extends Factory<Foo>() {
+ *      async init() {}
+ * }
+ *
+ *
+ *
+ * The returned class:
+ * @example
+ * abstract class Factory<T> {
+    static instanceList: T[] = [];
+    static createCount: number = 0;
+
+    static create(): T {
+        const instance = new T();
+
+        instance.id = this.createCount++;
+        instance.init();
+        this.instanceList.push(instance);
+
+        return instance;
+    }
+
+    static createList(amount = 0): T[] {
+        const instanceList: T[] = [];
+
+        for (let i = 0; i < amount; i++) instanceList.push(this.create());
+
+        return instanceList;
+    }
+
+    readonly id = 0;
+
+    abstract init(): void | Promise<void>;
+
+    destroy(): void {
+        this.constructor.instanceList.splice(this.constructor.instanceList.indexOf(this), 1);
+    }
+
+}
+ */
 Object.defineProperty(exports, "__esModule", { value: true });
 function Factory() {
-    var _a;
-    return _a = class Constructable {
-            constructor() {
-                this.id = 0;
-            }
-            static create() {
-                const instance = new this();
-                instance.id = this.createCount++;
-                //@ts-ignore
-                this.instanceList.push(instance);
-                //@ts-ignore
-                return instance;
-            }
-            static createList(amount = 0) {
-                const instanceList = [];
-                for (let i = 0; i < amount; i++)
-                    instanceList.push(this.create());
-                return instanceList;
-            }
-            destroy() {
-                this.constructor.instanceList.splice(this.constructor.instanceList.indexOf(this), 1);
-            }
-        },
-        _a.instanceList = [],
-        _a.createCount = 0,
-        _a;
+    class Factory {
+        constructor() {
+            this.id = 0;
+        }
+        static create() {
+            const instance = new this();
+            instance.id = this.createCount++;
+            instance.init();
+            //@ts-ignore
+            this.instanceList.push(instance);
+            //@ts-ignore
+            return instance;
+        }
+        static createList(amount = 0) {
+            const instanceList = [];
+            for (let i = 0; i < amount; i++)
+                instanceList.push(this.create());
+            return instanceList;
+        }
+        /**
+         * Retrive the reference of this instance from static class array instanceList.
+         */
+        destroy() {
+            this.constructor.instanceList.splice(this.constructor.instanceList.indexOf(this), 1);
+        }
+    }
+    Factory.instanceList = [];
+    Factory.createCount = 0;
+    return Factory;
 }
 exports.default = Factory;
--- src/factory/factory.ts
+++ src/factory/factory.ts
@@ -1,11 +1,61 @@
+/**
+ * Returns an abstract class that will add methods to manage instantiation and access to your class instances.
+ * Your instance will inherit the destroy method that will retrive the reference from the instance array so
+ * it would be deleted by the garbage collector as you make sure that there is no other refence to it.
+ * 
+ * Your class has to implement init for what you wish to do at instantiation time as you will not use the constructor.
+ * @example
+ * class Foo extends Factory<Foo>() {
+ *      async init() {}
+ * }
+ * 
+ * 
+ * 
+ * The returned class:
+ * @example
+ * abstract class Factory<T> {
+    static instanceList: T[] = [];
+    static createCount: number = 0;
+
+    static create(): T {
+        const instance = new T();
+
+        instance.id = this.createCount++;
+        instance.init();
+        this.instanceList.push(instance);
+
+        return instance;
+    }
+
+    static createList(amount = 0): T[] {
+        const instanceList: T[] = [];
+
+        for (let i = 0; i < amount; i++) instanceList.push(this.create());
+
+        return instanceList;
+    }
+
+    readonly id = 0;
+
+    abstract init(): void | Promise<void>;
+
+    destroy(): void {
+        this.constructor.instanceList.splice(this.constructor.instanceList.indexOf(this), 1);
+    }
+
+}
+ */
+
 export default function Factory<T>() {
-    return class Constructable {
+    abstract class Factory {
         static instanceList: T[] = [];
         static createCount: number = 0;
 
         static create(): T {
-            const instance =  new this();
+            const instance =  new (this as any)();
             (instance as any).id = this.createCount++;
+
+            instance.init();
             
             //@ts-ignore
             this.instanceList.push(instance);
@@ -22,10 +72,21 @@ export default function Factory<T>() {
             return instanceList;
         }
 
-        public readonly id = 0;
+        readonly id = 0;
 
-        public destroy(): void {
+        /**
+         * Will be called automatically with the static create method of the class after the attribution of the instance ID.
+         */
+        abstract init(): void | Promise<void>;
+
+        /**
+         * Retrive the reference of this instance from static class array instanceList.
+         */
+        destroy(): void {
             (this.constructor as any).instanceList.splice((this.constructor as any).instanceList.indexOf(this), 1);
         }
+
     }
+
+    return Factory;
 }
